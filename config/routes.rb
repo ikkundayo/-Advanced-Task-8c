f315b267753b4fa58a89decce7171b065a629907
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-03-09 09:50:43 +0000

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
     get :followeds, on: :member
   end
 
-  resources :groups, except: [:destroy]
+  resources :groups do
+    get "join" => "groups#join"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
